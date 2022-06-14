25ccb5ff8190d74d0ca565bea93f6b8147ead32c
wendyasada12 <wendyasada4@gmail.com>
2022-06-14 18:49:11 +0800

    Add Data Binding

--- app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/BookFavAdapter.kt
+++ app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/BookFavAdapter.kt
@@ -3,39 +3,42 @@ package com.ubaya.a160419062_ubayalibrary.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.a160419062_ubayalibrary.R
+import com.ubaya.a160419062_ubayalibrary.databinding.BookListItemBinding
 import com.ubaya.a160419062_ubayalibrary.model.Book
 import com.ubaya.a160419062_ubayalibrary.util.loadImage
 import kotlinx.android.synthetic.main.book_list_item.view.*
 
-class BookFavAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<BookFavAdapter.BookViewHolder>() {
-    class BookViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+class BookFavAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<BookFavAdapter.BookViewHolder>(), ButtonDetailClickListener{
+    class BookViewHolder(var view: BookListItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookFavAdapter.BookViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.book_list_item, parent, false)
+        val view = DataBindingUtil.inflate<BookListItemBinding>(inflater,R.layout.book_list_item, parent, false)
         return BookFavAdapter.BookViewHolder(view)
     }
     override fun onBindViewHolder(holder: BookFavAdapter.BookViewHolder, position: Int) {
-        val book = bookList[position]
-        with (holder.view){
-            textID.text = book.id
-            textBName.text = book.name
-            textAuthorName.text = book.author
-            buttonDetail.setOnClickListener {
-                val action = book.id?.let { id ->
-                    FavoriteFragmentDirections.actionFavToBookDetail(id)
-
-                }
-                if (action != null)
-                {
-                    Navigation.findNavController(it).navigate(action)
-                }
-            }
-            imageBookPhoto.loadImage(book.image, progressLoadingBookPhoto)
-        }
+        holder.view.books = bookList[position]
+//        val book = bookList[position]
+//        with (holder.view){
+//            textID.text = book.id
+//            textBName.text = book.name
+//            textAuthorName.text = book.author
+//            buttonDetail.setOnClickListener {
+//                val action = book.id?.let { id ->
+//                    FavoriteFragmentDirections.actionFavToBookDetail(id)
+//
+//                }
+//                if (action != null)
+//                {
+//                    Navigation.findNavController(it).navigate(action)
+//                }
+//            }
+//            imageBookPhoto.loadImage(book.image, progressLoadingBookPhoto)
+//        }
     }
 
     override fun getItemCount() = bookList.size
@@ -44,5 +47,9 @@ class BookFavAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<BookF
         bookList.clear()
         bookList.addAll(newBookList)
         notifyDataSetChanged()
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/BookLikeAdapter.kt
+++ app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/BookLikeAdapter.kt
@@ -3,40 +3,43 @@ package com.ubaya.a160419062_ubayalibrary.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ubaya.a160419062_ubayalibrary.R
+import com.ubaya.a160419062_ubayalibrary.databinding.BookListItemBinding
 import com.ubaya.a160419062_ubayalibrary.model.Book
 import com.ubaya.a160419062_ubayalibrary.util.loadImage
 import kotlinx.android.synthetic.main.book_list_item.view.*
 
-class BookLikeAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<BookLikeAdapter.BookViewHolder>() {
-    class BookViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+class BookLikeAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<BookLikeAdapter.BookViewHolder>(),ButtonDetailClickListener {
+    class BookViewHolder(var view: BookListItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookLikeAdapter.BookViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.book_list_item, parent, false)
+        val view = DataBindingUtil.inflate<BookListItemBinding>(inflater,R.layout.book_list_item, parent, false)
         return BookLikeAdapter.BookViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: BookLikeAdapter.BookViewHolder, position: Int) {
-        val book = bookList[position]
-        with (holder.view){
-            textID.text = book.id
-            textBName.text = book.name
-            textAuthorName.text = book.author
-            buttonDetail.setOnClickListener {
-                val action = book.id?.let { id ->
-                    LikeFragmentDirections.actionLikeToBookDetail(id)
-
-                }
-                if (action != null)
-                {
-                    Navigation.findNavController(it).navigate(action)
-                }
-            }
-            imageBookPhoto.loadImage(book.image, progressLoadingBookPhoto)
-        }
+        holder.view.books = bookList[position]
+//        val book = bookList[position]
+//        with (holder.view){
+//            textID.text = book.id
+//            textBName.text = book.name
+//            textAuthorName.text = book.author
+//            buttonDetail.setOnClickListener {
+//                val action = book.id?.let { id ->
+//                    LikeFragmentDirections.actionLikeToBookDetail(id)
+//
+//                }
+//                if (action != null)
+//                {
+//                    Navigation.findNavController(it).navigate(action)
+//                }
+//            }
+//            imageBookPhoto.loadImage(book.image, progressLoadingBookPhoto)
+//        }
     }
 
     override fun getItemCount() = bookList.size
@@ -45,5 +48,9 @@ class BookLikeAdapter(val bookList: ArrayList<Book>) : RecyclerView.Adapter<Book
         bookList.clear()
         bookList.addAll(newBookList)
         notifyDataSetChanged()
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        TODO("Not yet implemented")
     }
 }
