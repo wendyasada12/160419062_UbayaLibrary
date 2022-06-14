a60727307de396bb48d50dced388542b3b44ebb3
wendyasada12 <wendyasada4@gmail.com>
2022-06-14 16:55:45 +0800

    Add Data Binding

--- app/src/main/java/com/ubaya/a160419062_ubayalibrary/util/util.kt
+++ app/src/main/java/com/ubaya/a160419062_ubayalibrary/util/util.kt
@@ -3,10 +3,16 @@ package com.ubaya.a160419062_ubayalibrary.util
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import com.ubaya.a160419062_ubayalibrary.R
 import java.lang.Exception
+
+@BindingAdapter("android:imageUrl", "android:progressBar")
+fun loadImageFromUrl(view: ImageView, url: String, progressBar: ProgressBar) {
+    view.loadImage(url,progressBar)
+}
 
 fun ImageView.loadImage(url: String?, progressBar: ProgressBar) {
     Picasso.get()
