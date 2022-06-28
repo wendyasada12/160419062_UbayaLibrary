cddb0187153bff473e08121bb757352a9cb42d4d
eduardwilliam <willitairas@gmail.com>
2022-06-28 13:27:26 +0700

    Create Layout (Belum Rampung)

---
+++ app/src/main/java/com/ubaya/a160419062_ubayalibrary/model/BookDao.kt
@@ -0,0 +1,19 @@
+package com.ubaya.a160419062_ubayalibrary.model
+
+import androidx.room.*
+
+@Dao
+interface BookDao {
+    //Book
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBooks(vararg tenant:Book)
+
+    @Query("SELECT * FROM books")
+    suspend fun selectAllBooks(): List<Book>
+
+    @Query("SELECT * FROM books where id= :id")
+    suspend fun selectBooks(id:String): Book
+
+    @Delete
+    suspend fun deleteBooks(tenant: Book)
+}
