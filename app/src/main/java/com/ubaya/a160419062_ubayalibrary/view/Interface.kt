c77988834bc96ac5c3d73bbf7d0bf6efb04e1d1e
wendyasada12 <wendyasada4@gmail.com>
2022-06-30 23:03:46 +0800

    Fix Create dan edit + add databinding

--- app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/Interface.kt
+++ app/src/main/java/com/ubaya/a160419062_ubayalibrary/view/Interface.kt
@@ -1,6 +1,15 @@
 package com.ubaya.a160419062_ubayalibrary.view
 
 import android.view.View
+import android.widget.CompoundButton
+import com.ubaya.a160419062_ubayalibrary.model.Book
+
+interface TodoCheckedChangeListener {
+    fun onCheckChanged(cb: CompoundButton,
+                       isChecked:Boolean,
+                       obj: Book
+    )
+}
 
 interface ButtonDetailClickListener{
     fun onButtonDetailClick(v: View)
@@ -28,4 +37,16 @@ interface ButtonBlackClickListener{
 
 interface ButtonAddReviewClickListener{
     fun onButtonAddReview(v: View)
+}
+
+interface ButtonCreateBookClickListener{
+    fun onButtonCreateClick(v: View)
+}
+
+interface ButtonEditBookClick{
+    fun onButtonEditClick(v: View)
+}
+
+interface RadioClick {
+    fun onRadioClick(v:View, priority:Int, obj:Book)
 }
